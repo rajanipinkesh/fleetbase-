d8802c65ac02cb66e2886604f22e73de38cab547
Prasad Oturkar <prasad.oturkar@attomdata.com>
2021-12-23 12:27:03 +0530

    working

--- getip/IPGetter.php
+++ getip/IPGetter.php
@@ -18,3 +18,7 @@ class IPGetter
 
 $ip_address = IPGetter::getIPAddress();
 echo($ip_address);exit;
+
+
+git remote add origin https://github.com/panchalamit/Welcome-form.git
+git remote set-url origin https://github.com/panchalamit/Welcome-form.git
